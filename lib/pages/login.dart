339748c669bfbcb85cf5305118439aef4202591e
Rômulo Ferreira <romulo.ferreira.ufrpe@gmail.com>
2019-04-16 21:10:24 -0300

    SignUp_page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:historias/ui/home.dart';
+import 'signUp.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -12,7 +13,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   final _formKey = GlobalKey<FormState>();
   TextEditingController _emailTextController = TextEditingController();
   TextEditingController _passwordTextController = TextEditingController();
@@ -24,148 +25,173 @@ class _LoginState extends State<Login> {
   @override
   void initState() {
     super.initState();
-    isSignedIn();
+//    isSignedIn();
   }
+
+  void isSignedIn() async {
+    setState(() {
+      loading = true;
+    });
+
+    if (isLogedin) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    }
+
+    setState(() {
+      loading = false;
+    });
+  }
+
+//  Future handleSignIn() async {
+//    setState(() {
+//      loading = true;
+//    });
+//  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
       body: Stack(
         children: <Widget>[
           Image.asset(
-            "assets/img/bg_logiin.jpg",
+            "assets/img/bg_logiiin.jpg",
             fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.4),
             width: double.infinity,
             height: double.infinity,
           ),
           Padding(
             padding: const EdgeInsets.all(30.0),
             child: Container(
-              alignment: Alignment.topCenter,
-              child: Image.asset(
-                "assets/img/logo.png",
-                width: 280.0,
-                height: 240.0,
-              ),
-            ),
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  "assets/img/logo.png",
+                  width: 280.0,
+                  height: 240.0,
+                )),
           ),
-          Container(
-            color: Colors.black.withOpacity(0.6),
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 280.0),
-            child: Center(
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
-                      child: Material(
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Colors.white.withOpacity(0.8),
-                          elevation: 0.0,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 12.0),
-                            child: TextFormField(
-                              controller: _emailTextController,
-                              decoration: InputDecoration(
-                                hintText: "Email",
-                                icon: Icon(Icons.email),
-                              ),
-                              validator: (value) {
-                                if (value.isEmpty) {
-                                  Pattern pattern =
-                                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-                                  RegExp regex = new RegExp(pattern);
-                                  if (!regex.hasMatch(value)) {
-                                    return "Porfavor entre com um e-mail válido!";
-                                  } else {
-                                    return null;
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 280.0),
+              child: Center(
+                child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white.withOpacity(0.4),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: TextFormField(
+                                controller: _emailTextController,
+                                decoration: InputDecoration(
+                                  hintText: "Email",
+                                  icon: Icon(Icons.alternate_email),
+                                ),
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    Pattern pattern =
+                                        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                                    RegExp regex = new RegExp(pattern);
+                                    if (!regex.hasMatch(value))
+                                      return 'Entre com um email válido';
+                                    else
+                                      return null;
                                   }
-                                }
-                              },
+                                },
+                              ),
                             ),
-                          )),
-                    ),
-//                   ===================== Field Password
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
-                      child: Material(
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: Colors.white.withOpacity(0.8),
-                        elevation: 0.0,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 12.0),
-                          child: TextFormField(
-                            controller: _passwordTextController,
-                            decoration: InputDecoration(
-                              hintText: "Senha",
-                              icon: Icon(Icons.lock_outline),
-                            ),
-                            validator: (value) {
-                              if (value.isEmpty) {
-                                return "O campo senha esta vazio";
-                              } else if (value.length < 6) {
-                                return "A senha precisa conter mais de 6 caracteres";
-                              }
-                              return null;
-                            },
                           ),
                         ),
-                      ),
-                    ),
-// ============================== BUTTON LOGIN
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
-                      child: Material(
-                        borderRadius: BorderRadius.circular(10.0),
-                        color: Colors.orange.shade700,
-                        elevation: 0.0,
-                        child: MaterialButton(
-                          onPressed: () {},
-                          minWidth: MediaQuery.of(context).size.width,
+
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white.withOpacity(0.4),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: TextFormField(
+                                controller: _passwordTextController,
+                                decoration: InputDecoration(
+                                  hintText: "Senha",
+                                  icon: Icon(Icons.lock_outline),
+                                ),
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    return "O campo senha está vazio";
+                                  } else if (value.length < 6) {
+                                    return "A senha Precissa conter mais que 6 caracteres";
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: Colors.orange.shade700,
+                              elevation: 0.0,
+                              child: MaterialButton(
+                                onPressed: () {},
+                                minWidth: MediaQuery.of(context).size.width,
+                                child: Text(
+                                  "Entrar",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0),
+                                ),
+                              )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
                           child: Text(
-                            "Login",
+                            "Esqueceu a Senha ?",
                             textAlign: TextAlign.center,
                             style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22.0),
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Esqueceu a Senha ?",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white, fontWeight: FontWeight.w400),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: RichText(
-                        text: TextSpan(
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w400,
-                                fontSize: 16.0),
-                            children: [
-                              TextSpan(text: "Ainda não tem conta? "),
-                              TextSpan(
-                                  text: "Cadastre-se",
-                                  style: TextStyle(color: Colors.red))
-                            ]),
-                      ),
-                    ),
-                  ],
-                ),
+//                          Expanded(child: Container()),
+
+                        Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => SignUp()));
+                                },
+                                child: Text(
+                                  "Cadastre-se",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.orange, fontSize: 19.0),
+                                ))),
+                      ],
+                    )),
               ),
             ),
           ),
@@ -180,24 +206,9 @@ class _LoginState extends State<Login> {
                 ),
               ),
             ),
-          ),
+          )
         ],
       ),
     );
-  }
-
-  void isSignedIn() async {
-    setState(() {
-      loading = true;
-    });
-    preferences = await SharedPreferences.getInstance();
-    if (isLogedin) {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => HomePage()));
-    }
-
-    setState(() {
-      loading = false;
-    });
   }
 }
---
+++ lib/pages/signUp.dart
@@ -0,0 +1,247 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _passwordConfirmTextController =
+      TextEditingController();
+
+  String gender;
+  String groupValue = "male";
+
+  bool loading = false;
+  bool hidePass = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            "assets/img/bg_logiin.jpg",
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.4),
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Container(
+                alignment: Alignment.topCenter,
+                child: Image.asset(
+                  "assets/img/logo.png",
+                  width: 280.0,
+                  height: 240.0,
+                )),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 280.0),
+              child: Center(
+                child: Form(
+                    key: _formKey,
+                    child: ListView(
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white.withOpacity(0.4),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: TextFormField(
+                                controller: _nameTextController,
+                                decoration: InputDecoration(
+                                    hintText: "Nome",
+                                    icon: Icon(FontAwesomeIcons.user),
+                                    border: InputBorder.none),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white.withOpacity(0.4),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: TextFormField(
+                                controller: _emailTextController,
+                                decoration: InputDecoration(
+                                    hintText: "Email",
+                                    icon: Icon(Icons.alternate_email),
+                                    border: InputBorder.none),
+                                validator: (value) {
+                                  if (value.isEmpty) {
+                                    Pattern pattern =
+                                        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                                    RegExp regex = new RegExp(pattern);
+                                    if (!regex.hasMatch(value))
+                                      return 'Entre com um email válido';
+                                    else
+                                      return null;
+                                  }
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white.withOpacity(0.4),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: ListTile(
+                                title: TextFormField(
+                                  controller: _passwordTextController,
+                                  obscureText: hidePass,
+                                  decoration: InputDecoration(
+                                      hintText: "Senha",
+                                      icon: Icon(Icons.lock_outline),
+                                      border: InputBorder.none),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return "O campo senha está vazio";
+                                    } else if (value.length < 6) {
+                                      return "A senha Precissa conter mais que 6 caracteres";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                trailing: IconButton(
+                                  icon: Icon(FontAwesomeIcons.eye),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (hidePass == true) {
+                                        hidePass = false;
+                                      } else if (hidePass == false) {
+                                        hidePass = true;
+                                      }
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.white.withOpacity(0.4),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 12.0),
+                              child: ListTile(
+                                title: TextFormField(
+                                  controller: _passwordConfirmTextController,
+                                  obscureText: hidePass,
+                                  decoration: InputDecoration(
+                                      hintText: "Entre com a senha novamente",
+                                      icon: Icon(Icons.lock_outline),
+                                      border: InputBorder.none),
+                                  validator: (value) {
+                                    if (value.isEmpty) {
+                                      return "O campo senha está vazio";
+                                    } else if (value.length < 6) {
+                                      return "A senha Precissa conter mais que 6 caracteres";
+                                    } else if (_passwordTextController.text !=
+                                        value) {
+                                      return "As senhas não são iguais";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                trailing: IconButton(
+                                  icon: Icon(FontAwesomeIcons.eye),
+                                  onPressed: () {
+                                    setState(() {
+                                      if (hidePass == true) {
+                                        hidePass = false;
+                                      } else if (hidePass == false) {
+                                        hidePass = true;
+                                      }
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(14.0, 8.0, 14.0, 8.0),
+                          child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: Colors.orange.shade700,
+                              elevation: 0.0,
+                              child: MaterialButton(
+                                onPressed: () {},
+                                minWidth: MediaQuery.of(context).size.width,
+                                child: Text(
+                                  "Criar Conta",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0),
+                                ),
+                              )),
+                        ),
+                      ],
+                    )),
+              ),
+            ),
+          ),
+          Visibility(
+            visible: loading ?? true,
+            child: Center(
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.white.withOpacity(0.9),
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  valueChanged(e) {
+    setState(() {
+      if (e == "male") {
+        groupValue = e;
+      } else if (e == "female") {
+        groupValue = e;
+      }
+    });
+  }
+}
