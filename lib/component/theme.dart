6bf528bfd33c14f8a0b3ff07526c1576228162e1
Rômulo Ferreira <romulo.ferreira.ufrpe@gmail.com>
2019-05-22 09:07:56 -0300

    CountImagesFall

--- lib/component/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-import 'package:flutter/cupertino.dart';
-
-class Colors{
-  const Colors();
-
-  static const Color backDrawerColor = const Color(0XFFF5D372);
-  static const Color appBarColor = const Color(0xFFE28935);
-}
